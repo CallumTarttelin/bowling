708df51dddcd28e2d359b1c736b20ad3e9b9f75b
Callum Tarttelin <callum@tarttelin.co.uk>
2018-02-17 22:09:20 +0000

    Update images to adoc.py

--- images to adoc.py
+++ images to adoc.py
@@ -12,16 +12,13 @@ def get_adoc(dr):
 
 
 def adocify(name, directory):
-    return """
-    .{1}
-    image:{0}[{1},pdfwidth=100%]
-    """.format(directory + name, name[:-4])
+    return "\n.{1}\nimage:{0}[{1},pdfwidth=100%]\n".format(directory + name, name[:-4])
 
 
 def prettify(inp):
     string = ""
     for key in inp:
-        string += "== {0}\n=== Test Results \n\n\n===Pictures ".format(key)
+        string += "== {0}\n=== Test Results \n\n\n=== Pictures \n".format(key)
         for adoc in inp.get(key):
             string += adoc
         string += "\n\n\n"
