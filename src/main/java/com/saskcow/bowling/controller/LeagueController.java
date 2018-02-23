6562e55d5ef2927ff10f6028cd26d569d2dc55c4
Saskcow <callum@tarttelin.co.uk>
2018-02-23 23:31:47 +0000

    small changes to league controller stuff

--- src/main/java/com/saskcow/bowling/controller/LeagueController.java
+++ src/main/java/com/saskcow/bowling/controller/LeagueController.java
@@ -5,41 +5,39 @@ import com.saskcow.bowling.repository.LeagueRepository;
 import com.saskcow.bowling.view.LeagueView;
 import com.saskcow.bowling.view.LeagueViewSummary;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ResourceNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class LeagueController {
 
     private LeagueRepository repo;
-//    private TeamRepository teamRepository;
 
     @Autowired
+    // This happens
     public LeagueController(LeagueRepository repo){
         this.repo = repo;
     }
 
     @RequestMapping(value = "/api/league", method = RequestMethod.GET)
-    public ResponseEntity<Iterable<LeagueViewSummary>> findLeagues() {
-        List<LeagueViewSummary> leagues = new ArrayList<>();
-        repo.findAll().
-                forEach(league -> leagues.add(new LeagueViewSummary(league)));
+    public ResponseEntity<List<LeagueViewSummary>> findLeagues() {
+        List<LeagueViewSummary> leagues = StreamSupport.stream(repo.findAll().spliterator(), false)
+                .map(LeagueViewSummary::new).collect(Collectors.toList());
         return ResponseEntity.ok(leagues);
     }
 
     @RequestMapping(value = "/api/league", method = RequestMethod.GET, params = "name")
-    public ResponseEntity<Iterable<LeagueViewSummary>> findLeaguesByName(@RequestParam("name") String name) {
-        List<LeagueViewSummary> leagues = new ArrayList<>();
-        repo.findByNameContaining(name).
-                forEach(league -> leagues.add(new LeagueViewSummary(league)));
+    public ResponseEntity<List<LeagueViewSummary>> findLeaguesByName(@RequestParam("name") String name) {
+        List<LeagueViewSummary> leagues = repo.findByNameContaining(name).stream().map(LeagueViewSummary::new).collect(Collectors.toList());
         return ResponseEntity.ok(leagues);
     }
 
@@ -65,11 +63,9 @@ public class LeagueController {
     @RequestMapping(value = "/api/league/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteLeague(@PathVariable("id") Long id) {
         try {
-//            League league = repo.findOne(id);
             repo.delete(id);
-//            league.getTeams().forEach();
             return ResponseEntity.noContent().build();
-        } catch (ResourceNotFoundException e) {
+        } catch (EmptyResultDataAccessException e) {
             return ResponseEntity.notFound().build();
         }
     }
