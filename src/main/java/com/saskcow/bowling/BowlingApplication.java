63a03e3ec7ae56d4d07c17377026e0289e8a60fb
saskcow <callum@tarttelin.co.uk>
2018-03-29 23:23:31 +0100

    Init copy paste fix weird git stuff.

---
+++ src/main/java/com/saskcow/bowling/BowlingApplication.java
@@ -0,0 +1,12 @@
+package com.saskcow.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingApplication.class, args);
+	}
+}
